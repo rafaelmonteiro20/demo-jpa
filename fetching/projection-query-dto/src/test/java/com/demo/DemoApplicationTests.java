3e0d7be880ae6f8589203eaeb2a4844e4c6f1b1d
Rafael Monteiro <rafaelmonteiro.developer@gmail.com>
2018-10-03 22:38:48 -0300

    Testing with constructor expression and native querie

--- fetching/projection-query-dto/src/test/java/com/demo/DemoApplicationTests.java
+++ fetching/projection-query-dto/src/test/java/com/demo/DemoApplicationTests.java
@@ -1,7 +1,6 @@
 package com.demo;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.*;
 
 import java.util.List;
 
@@ -16,6 +15,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.demo.model.Post;
+import com.demo.model.PostDTO;
 
 /**
  * https://vladmihalcea.com
@@ -46,6 +46,45 @@ public class DemoApplicationTests {
 				 
 		Tuple postDTO = postDTOs.get(0);
 		assertEquals("High-Performance Java Persistence", postDTO.get("title"));
+	}
+	
+	@Test
+	public void testWithConstructorExpression() {
+		
+		Post post = new Post();
+		post.setCreateBy("Rafael Monteiro");
+		post.setTitle("High-Performance Java Persistence");
+		post.setUpdateBy("Rafael Monteiro");
+		manager.persist(post);
+		
+		List<PostDTO> postDTOs = manager.createQuery(
+			"select new com.demo.model.PostDTO(" +
+			"	p.id, p.title) from Post p ",
+			PostDTO.class).getResultList();
+		
+		PostDTO postDTO = postDTOs.get(0);
+		assertEquals("High-Performance Java Persistence", postDTO.getTitle());
+	}
+	
+	@Test
+	public void testWithTupleAndNativeQuerie() {
+		
+		Post post = new Post();
+		post.setCreateBy("Rafael Monteiro");
+		post.setTitle("High-Performance JPA");
+		post.setUpdateBy("Rafael Monteiro");
+		manager.persist(post);
+		
+		@SuppressWarnings("unchecked")
+		List<Tuple> postDTOs = manager.createNativeQuery(
+			"SELECT " +
+			"	p.id AS id, " +
+			"   p.title AS title " +
+			"FROM Post p ",
+			Tuple.class).getResultList();
+				 
+		Tuple postDTO = postDTOs.get(0);
+		assertEquals("High-Performance JPA", postDTO.get("title"));
 	}
 	
 }
