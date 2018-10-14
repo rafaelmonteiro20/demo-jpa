77045b07d03b84c2e1926cdccb9750d886b35140
Rafael Monteiro <rafaelmonteiro.developer@gmail.com>
2018-10-14 11:11:03 -0300

    Testing converter and embeddable

--- advanced-mapping/advanced-orm/src/test/java/com/demo/DemoApplicationTests.java
+++ advanced-mapping/advanced-orm/src/test/java/com/demo/DemoApplicationTests.java
@@ -1,5 +1,9 @@
 package com.demo;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
@@ -7,6 +11,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.demo.model.ContactInfo;
+import com.demo.model.Employee;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -16,8 +23,24 @@ public class DemoApplicationTests {
 	private EntityManager manager;
 	
 	@Test
-	public void test() {
+	public void testConverter() {
 	
+		List<Employee> employees = manager.createQuery(
+			"select e from Employee e where e.active = true", Employee.class)
+			.getResultList();
+		
+		assertEquals(4, employees.size());
+	}
+	
+	@Test
+	public void testEmbeddable() {
+		
+		List<ContactInfo> employees = manager.createQuery(
+				"select e.contactInfo from Employee e " +
+				"where e.contactInfo.address.state = 'NY'", ContactInfo.class)
+				.getResultList();
+		
+		assertEquals(2, employees.size());
 	}
 
 }
